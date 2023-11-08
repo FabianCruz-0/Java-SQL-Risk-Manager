3c38569ee724dd47b1151e213091a7d25fed7516
Fabián Cruz <fabiancruz.dev@gmail.com>
2023-11-07 22:44:29 -0600

    all delete buttons and logic

--- src/main/java/src/javasqlriskmanager/controllers/rolcontrollers/DetalleRolController.java
+++ src/main/java/src/javasqlriskmanager/controllers/rolcontrollers/DetalleRolController.java
@@ -1,17 +1,24 @@
 package src.javasqlriskmanager.controllers.rolcontrollers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import org.w3c.dom.Text;
 import src.javasqlriskmanager.MainApplication;
 import src.javasqlriskmanager.models.Rol;
 import src.javasqlriskmanager.singletons.RolSingleton;
+import src.javasqlriskmanager.singletons.SesionSingleton;
+import src.javasqlriskmanager.singletons.UserSingleton;
+import src.javasqlriskmanager.utils.ConnectToDB;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.ResourceBundle;
 
 import static src.javasqlriskmanager.MainApplication.principalStage;
@@ -40,5 +47,45 @@ public class DetalleRolController implements Initializable {
 
         id.setText(rol.getID().toString());
         name.setText(rol.getName());
+    }
+
+    public void delete() throws IOException {
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setHeaderText(null);
+        alert.setTitle("Confirmación de eliminar.");
+        alert.setContentText("¿Desea confirmar proceder con la eliminación del registro?");
+        alert.showAndWait();
+
+        if(alert.getResult().getText().equals("Aceptar")) {
+
+            if(RolSingleton.getInstance().getRol().getID().equals(SesionSingleton.getInstance().getUsuario().getID_Role())) {
+                alert = new Alert(Alert.AlertType.ERROR);
+                alert.setHeaderText(null);
+                alert.setTitle("Error");
+                alert.setContentText("No puedes eliminar el rol de adminisitrador.");
+                alert.showAndWait();
+            } else {
+                try {
+                    Connection con = ConnectToDB.connectToDB();
+
+                    String query = "UPDATE Users SET ID_Role = NULL WHERE ID_Role = ?";
+                    PreparedStatement preparedStatement = con.prepareStatement(query);
+                    preparedStatement.setLong(1, RolSingleton.getInstance().getRol().getID());
+                    preparedStatement.execute();
+
+                    query = "DELETE FROM User_Roles WHERE ID = ?";
+                    preparedStatement = con.prepareStatement(query);
+                    preparedStatement.setLong(1, RolSingleton.getInstance().getRol().getID());
+                    preparedStatement.execute();
+
+                    con.close();
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                    e.printStackTrace();
+                }
+                irCatalogo();
+            }
+        }
     }
 }
--- src/main/java/src/javasqlriskmanager/controllers/userscontrollers/DetalleUsuarioController.java
+++ src/main/java/src/javasqlriskmanager/controllers/userscontrollers/DetalleUsuarioController.java
@@ -10,6 +10,7 @@ import javafx.scene.control.TextField;
 import src.javasqlriskmanager.MainApplication;
 import src.javasqlriskmanager.models.Usuario;
 import src.javasqlriskmanager.singletons.IncidentSingleton;
+import src.javasqlriskmanager.singletons.SesionSingleton;
 import src.javasqlriskmanager.singletons.UserSingleton;
 import src.javasqlriskmanager.utils.ConnectToDB;
 
@@ -78,31 +79,38 @@ public class DetalleUsuarioController implements Initializable {
 
         if(alert.getResult().getText().equals("Aceptar")) {
 
-            try {
-                Connection con = ConnectToDB.connectToDB();
+            if(UserSingleton.getInstance().getUsuario().getID().equals(SesionSingleton.getInstance().getUsuario().getID())) {
+                alert = new Alert(Alert.AlertType.ERROR);
+                alert.setHeaderText(null);
+                alert.setTitle("Error");
+                alert.setContentText("No puedes eliminar el usuario con con el que iniciaste sesión.");
+                alert.showAndWait();
+            } else {
+                try {
+                    Connection con = ConnectToDB.connectToDB();
 
-                String query = "UPDATE Incidents SET ID_CreatorUser = NULL WHERE ID_CreatorUser = ?";
-                PreparedStatement preparedStatement = con.prepareStatement(query);
-                preparedStatement.setLong(1, UserSingleton.getInstance().getUsuario().getID());
-                preparedStatement.execute();
+                    String query = "UPDATE Incidents SET ID_CreatorUser = NULL WHERE ID_CreatorUser = ?";
+                    PreparedStatement preparedStatement = con.prepareStatement(query);
+                    preparedStatement.setLong(1, UserSingleton.getInstance().getUsuario().getID());
+                    preparedStatement.execute();
 
-                query = "UPDATE Incidents SET ID_AssignedUser = NULL WHERE ID_AssignedUser = ?";
-                preparedStatement = con.prepareStatement(query);
-                preparedStatement.setLong(1, UserSingleton.getInstance().getUsuario().getID());
-                preparedStatement.execute();
+                    query = "UPDATE Incidents SET ID_AssignedUser = NULL WHERE ID_AssignedUser = ?";
+                    preparedStatement = con.prepareStatement(query);
+                    preparedStatement.setLong(1, UserSingleton.getInstance().getUsuario().getID());
+                    preparedStatement.execute();
 
-                query = "DELETE FROM Users WHERE ID = ?";
-                preparedStatement = con.prepareStatement(query);
-                preparedStatement.setLong(1, UserSingleton.getInstance().getUsuario().getID());
-                preparedStatement.execute();
+                    query = "DELETE FROM Users WHERE ID = ?";
+                    preparedStatement = con.prepareStatement(query);
+                    preparedStatement.setLong(1, UserSingleton.getInstance().getUsuario().getID());
+                    preparedStatement.execute();
 
-                con.close();
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-                e.printStackTrace();
+                    con.close();
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                    e.printStackTrace();
+                }
+                irCatalogo();
             }
-
-            irCatalogo();
         }
     }
 }
